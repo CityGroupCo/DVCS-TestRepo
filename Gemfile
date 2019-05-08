d98bc7cfb7ce515678f24165ab56f0f5154e4c04
nithin <nithin.p@experionglobal.com>
2019-05-08 14:33:40 +0530

    - initial commit

---
+++ Gemfile
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+git_source(:github) {TestApp "https://github.com/CityGroupCo/DVCS-TestRepo"}
+
+gem "danger"
+
--- app/src/main/java/com/example/testapplication/MainActivity.kt
+++ app/src/main/java/com/example/testapplication/MainActivity.kt
@@ -2,11 +2,16 @@ package com.example.testapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Toast.makeText(this, "dfkjnwe", Toast.LENGTH_SHORT).show()
+        //New line added.
+        //Test dangerdanger local
     }
 }
